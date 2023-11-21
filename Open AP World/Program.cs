b2ccb3c2be1658c9d33f37fdb3dc0abe1b570c06
Sean Gallagher <seansg725@gmail.com>
2023-11-21 08:27:46 -0500

    Add project files.

---
+++ Open AP World/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using OpenAI;
+using System.Reflection;
+using Spectre.Console;
+
+AnsiConsole.Markup("[orangered1]Hello World![/]");
