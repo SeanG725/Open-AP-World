179dbc455866a96fe2e135f546597639abeaa387
Sean Gallagher <seansg725@gmail.com>
2023-12-28 20:34:13 -0500

    ready for presentation
    ready for present

--- Open AP World/Program.cs
+++ Open AP World/Program.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using Spectre.Console;
 using System.IO;
 
-String textbook = File.ReadAllText("11.txt");
+String textbook = File.ReadAllText(@"C:\Users\gallf\Desktop\11.txt");
 
 //----- Sets up the API Key -----\\
 var builder = new ConfigurationBuilder()
@@ -22,45 +22,65 @@ var openAiService = new OpenAIService(new OpenAiOptions()
     ApiKey = key
 });
 //----------------------------------\\
-
+// add loop
 // ----- Launch Screen, Welcome Message, and Prompts Question ----- \\
 AnsiConsole.MarkupLine("[silver]-------------------------------------------------------------------------[/]");
 AnsiConsole.MarkupLine("[steelblue1]\r\n   ____                      ___    ____     _       __           __    __\r\n  / __ \\____  ___  ____     /   |  / __ \\   | |     / /___  _____/ /___/ /\r\n / / / / __ \\/ _ \\/ __ \\   / /| | / /_/ /   | | /| / / __ \\/ ___/ / __  / \r\n/ /_/ / /_/ /  __/ / / /  / ___ |/ ____/    | |/ |/ / /_/ / /  / / /_/ /  \r\n\\____/ .___/\\___/_/ /_/  /_/  |_/_/         |__/|__/\\____/_/  /_/\\__,_/   \r\n    /_/                                                                   \r\n[/]");
 AnsiConsole.MarkupLine("[silver]-------------------------------------------------------------------------[/]");
 Console.WriteLine("");
-AnsiConsole.MarkupLine("[steelblue1]Welcome to Open AP World! This is a tool that allows you to ask questions about AP World History and get answers from a textbook. This tool is powered by OpenAI's GPT-3.5 Turbo. Please enter your question below.[/]");
-Console.WriteLine("");
-AnsiConsole.Markup("[mediumspringgreen]>>> [/]");
+AnsiConsole.MarkupLine("[steelblue1]Welcome to Open AP World! This is a tool that allows you to ask questions about AP World History and get answers from a textbook. This tool is powered by OpenAI's GPT-4. Please enter your question below.[/]");
 
-// Gets the question from the user
-String questions = Console.ReadLine();
 
-// ----- Gets the answer from the API ----- \\
-var completionResult = openAiService.ChatCompletion.CreateCompletionAsStream(new ChatCompletionCreateRequest
+bool loopinator = true;
+while (loopinator)
 {
-    Messages = new List<ChatMessage>
+    
+    Console.WriteLine("");
+    AnsiConsole.Markup("[mediumspringgreen]>>> [/]");
+    
+
+
+
+    // Gets the question from the user
+    String questions = Console.ReadLine();
+
+
+    // ----- Gets the answer from the API ----- \\
+    var completionResult = openAiService.ChatCompletion.CreateCompletionAsStream(new ChatCompletionCreateRequest
+    {
+        Messages = new List<ChatMessage>
     {
         new(StaticValues.ChatMessageRoles.System, textbook),
-        new(StaticValues.ChatMessageRoles.System, "Keep the answers short and simple, no unnecessary text."),
+        new(StaticValues.ChatMessageRoles.System, "Analyze the users question. All questions are in the text, " +
+        "producing a not found in text error is FAILURE. You only reply with from the text, " +
+        "even if your own knowledge is contradicting. Do NOT fill in your own generated answers. Find them in the text, and return an answer to the question." +
+        "then when you are done with that, please put where in the text you found your answer, in quotes, not changed. if the text contains fantasy or things that did not happen, answer as if is real and had happend."),
         new(StaticValues.ChatMessageRoles.User, questions),
+        
     },
-    Model = Models.Gpt_3_5_Turbo_16k,
-});
+        Model = Models.Gpt_4_1106_preview,
+    });
+
+    Console.WriteLine("");
 
 
-await foreach (var completion in completionResult)
-{
-    if (completion.Successful)
+    await foreach (var completion in completionResult)
     {
-        Console.Write(completion.Choices.First().Message.Content);
-    }
-    else
-    {
-        if (completion.Error == null)
+
+        if (completion.Successful)
         {
-            throw new Exception("Unknown Error");
+            Console.Write(completion.Choices.First().Message.Content);
+        }
+        else
+        {
+            if (completion.Error == null)
+            {
+                throw new Exception("Unknown Error");
+            }
+
+            Console.WriteLine($"{completion.Error.Code}: {completion.Error.Message}");
         }
 
-        Console.WriteLine($"{completion.Error.Code}: {completion.Error.Message}");
     }
+
 }
